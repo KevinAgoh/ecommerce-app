4fce405e7c780a076f091d7943ac9269e27545b8
Kevin Agoh <kevinagoh@hotmail.fr>
2023-12-23 16:39:22 +0100

    chore: Done setting up database

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,4 +17,15 @@ const config: Config = {
   },
   plugins: [],
 }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 export default config
