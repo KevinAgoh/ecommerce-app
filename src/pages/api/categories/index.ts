77402a3fbf2789b1195080f880d517af04c19738
Kevin Agoh <kevinagoh@hotmail.fr>
2023-12-26 00:07:57 +0100

    api: setup API's for products and categories

---
+++ src/pages/api/categories/index.ts
@@ -0,0 +1,44 @@
+import { TApiAllCategoriesResp, TApiErrorResp } from "@/src/types";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (
+  _req: NextApiRequest,
+  res: NextApiResponse<TApiAllCategoriesResp | TApiErrorResp>
+) => {
+  if (_req.method !== 'GET') {
+    res.status(405).send({ message: 'Method Not Allowed' });
+    return;
+  }
+
+  try {
+    const categories = await prisma.category.findMany({
+      select: {
+        id: true,
+        name: true,
+        products: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          take: 8,
+          select: {
+            title: true,
+            description: true,
+            image: true,
+            price: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return res.json({ categories });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something went wrong!! Please try again after sometime",
+    });
+  }
+};
+
+
+export default handler;
