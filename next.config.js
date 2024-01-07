42a76c7b0275fac38d01024cea212f858a896275
Kevin Agoh <kevinagoh@hotmail.fr>
2024-01-08 00:20:41 +0100

    Fixed config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: ['placeimg.com', 'picsum.photos'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   },
-  distDir: '_next',
+  distDir: '.next',
   generateBuildId: async () => {
     if (process.env.BUILD_ID) {
       return process.env.BUILD_ID;
